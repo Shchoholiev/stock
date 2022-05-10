0d2efb9f503bb2421bc89361428e2107247f154b
DESKTOP-1P3UK3L\rareb <sh.codework@gmail.com>
2022-05-10 09:12:31 +0300

    Added generic repository.

---
+++ Stock/Stock.Application/IRepositories/IGenericRepository.cs
@@ -0,0 +1,18 @@
+﻿using Stock.Application.Paging;
+using Stock.Core.Entities;
+
+namespace Stock.Application.IRepositories
+{
+    public interface IGenericRepository<TEntity> where TEntity : EntityBase
+    {
+        Task AddAsync(TEntity entity);
+
+        Task UpdateAsync(TEntity entity);
+
+        Task DeleteAsync(TEntity entity);
+
+        Task<TEntity> GetOneAsync(int id);
+
+        Task<PagedList<TEntity>> GetPageAsync(PageParameters pageParameters);
+    }
+}
