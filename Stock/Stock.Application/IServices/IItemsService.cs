8884fbaf89ad4c5cbf641324d4878d323613fb05
DESKTOP-1P3UK3L\rareb <sh.codework@gmail.com>
2022-05-10 10:00:10 +0300

    Added Inteface for Items Service.

---
+++ Stock/Stock.Application/IServices/IItemsService.cs
@@ -0,0 +1,14 @@
+﻿using Stock.Application.Paging;
+using Stock.Core.Entities;
+
+namespace Stock.Application.IServices
+{
+    public interface IItemsService
+    {
+        Task<PagedList<Item>> GetItemsPageAsync(PageParameters pageParameters);
+
+        Task ProcessIncomeInvoiceAsync(IEnumerable<Item> items);
+
+        Task ProcessSalesInvoiceAsync(IEnumerable<Item> items);
+    }
+}
