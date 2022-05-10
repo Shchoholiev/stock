fb5e237cb94c38dce65f5a9f670358c6aa26fd30
DESKTOP-1P3UK3L\rareb <sh.codework@gmail.com>
2022-05-10 11:30:14 +0300

    Added EF context and DbInitializer.

---
+++ Stock/Stock.Infrastructure/EF/ApplicationContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Stock.Core.Entities;
+
+namespace Stock.Infrastructure.EF
+{
+    namespace EducationalPortal.Infrastructure.EF
+    {
+        public class ApplicationContext : DbContext
+        {
+            public ApplicationContext()
+            {
+            }
+
+            public ApplicationContext(DbContextOptions<ApplicationContext> options)
+                : base(options)
+            {
+            }
+
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                string connectionString = @"server=(LocalDb)\MSSQLLocalDB;database=Stock;integrated security=True;
+                    MultipleActiveResultSets=True;App=EntityFramework;";
+                optionsBuilder.UseSqlServer(connectionString);
+            }
+
+            public DbSet<Item> Items { get; set; }
+        }
+    }
+}
