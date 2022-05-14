2c2247f0fc21e0035c7e2e598e37e05f6fb7a7d6
DESKTOP-1P3UK3L\rareb <sh.codework@gmail.com>
2022-05-14 10:12:07 +0300

    Added Item DTO and Mapper.

---
+++ Stock/Stock.Application/DTOs/ItemDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace Stock.Application.DTOs
+{
+    public class ItemDTO
+    {
+        public string Name { get; set; }
+
+        public string Unit { get; set; }
+
+        public double Price { get; set; }
+
+        public int Count { get; set; }
+    }
+}
